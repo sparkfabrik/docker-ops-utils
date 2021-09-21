86183df15f546c57faa8fa8e609e4469645450c0
Daniele Monti <62102073+Monska85@users.noreply.github.com>
2021-09-21 11:38:50 +0200

    refs #000: manage the no items to delete case in aws/clean-bucket

--- app/commands/aws/subcommands/clean-bucket.sh
+++ app/commands/aws/subcommands/clean-bucket.sh
@@ -12,5 +12,15 @@ fi
 # All the required inputs are present! Do the job
 debug "All the required inputs are present. Go on with the real job."
 
-debug "Delete the file versions and the delete markers"
-aws s3api delete-objects --bucket "${BUCKET}" --delete "{\"Objects\": $(aws s3api list-object-versions --bucket "${BUCKET}" --output json | jq '. | to_entries | [.[] | select(.key | match("^Versions|DeleteMarkers$")) | .value[] | {Key:.Key,VersionId:.VersionId}]')}"
+debug "Start process of delete the file versions and the delete markers"
+
+debug "Get objects"
+ITEMS=$(aws s3api list-object-versions --bucket "${BUCKET}" --output json | jq '. | to_entries | [.[] | select(.key | match("^Versions|DeleteMarkers$")) | .value[] | {Key:.Key,VersionId:.VersionId}]')
+
+if [ -z "${ITEMS}" ]; then
+  debug "There is no items to delete"
+  exit 13
+fi
+
+debug "Delete objects"
+aws s3api delete-objects --bucket "${BUCKET}" --delete "{\"Objects\": ${ITEMS}}"
